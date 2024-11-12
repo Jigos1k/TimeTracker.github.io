cb9dff4df60472517c39b2f86ecfa52dc144a655
Jigos <nikitzhig6789@gmail.com>
2024-11-12 20:29:05 +0800

    add icons

--- TimeTracker/resources/views/welcome.blade.php
+++ TimeTracker/resources/views/welcome.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Time Tracker</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="form-control"><i class="bi bi-0-square-fill"></i>Привет <span class="badge text-bg-success">Всё успешно подключено</span></div>
+    <i class="bi bi-0-square-fill"></i>
+    <div class="form-control">Привет <span class="badge text-bg-success">Всё успешно подключено</span></div>
+    <i class="fa-solid fa-comment"></i>
 </body>
 </html>
