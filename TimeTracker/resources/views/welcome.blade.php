1faef642e00dcbb30b765e15428729c08a93eebf
Jigos <nikitzhig6789@gmail.com>
2024-11-12 20:10:44 +0800

    fix

--- TimeTracker/resources/views/welcome.blade.php
+++ TimeTracker/resources/views/welcome.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TimeTracker</title>
+    <title>Time Tracker</title>
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div class="form-control text-success">Привет</div>
+    <div class="form-control"><i class="bi bi-0-square-fill"></i>Привет <span class="badge text-bg-success">Всё успешно подключено</span></div>
 </body>
 </html>
