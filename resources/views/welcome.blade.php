d40dc5f2ba9f32b7c673cd2537eeb66cca8d9fa9
Jigos <nikitzhig6789@gmail.com>
2024-11-12 19:24:24 +0800

    Add Bootstrap and Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>TimeTracker</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="form-control text-success">Привет</div>
+</body>
+</html>
