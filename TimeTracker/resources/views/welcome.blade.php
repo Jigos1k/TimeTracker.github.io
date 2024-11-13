bd64a53efeaafdc2d72405a9e0f3d63244e36df0
Jigos <nikitzhig6789@gmail.com>
2024-11-13 20:13:45 +0800

    icons

--- TimeTracker/resources/views/welcome.blade.php
+++ TimeTracker/resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Time Tracker</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="{{ asset('bootstrap-icons/font/bootstrap-icons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('fontawesome-free/css/all.min.css') }}">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
